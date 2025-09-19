15381d6bf84c969e3fad7c3b062d6a9ae7f2ee4e
Iván Mañús Murcia <ivanmanyus@bit2me.com>
2025-09-19 10:18:24 +0200

    Update list all your txs with GET wallet/tx to v2

--- listTransactions.js
+++ listTransactions.js
@@ -4,14 +4,26 @@
  */
 
 // Bit2me logic
-const { getTx } = require('./utils/getTx')
+const axios  = require('axios');
+const { getAuthHeaders } = require('./bit2me_logic/utils')
+
+const PATH = process.env.END_GET_WALLET_TX;
 
 const args = process.argv.slice(2);
-
 const SUBACCOUNT = args[0]
 
 const listTransactions= async () => {
-    console.log(await getTx("", SUBACCOUNT))
+    try {
+        const response = await axios.get(
+            `${process.env.SERVER}${PATH}`,
+            getAuthHeaders(`${PATH}`, SUBACCOUNT)
+        );
+        console.log(response.data);
+    }
+    catch(e) {
+        console.error(e.response.data)
+        console.log("\n> Send reqId to Bit2Me team to debug it :)")
+    }
 }
 
 listTransactions()
